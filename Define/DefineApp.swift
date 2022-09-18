8588d2b41b1c3f038fa9eda32d2563f7b9ee441b
benshutt <ben@3sidedcube.com>
2022-09-18 19:54:07 +0100

    Initial commit

---
+++ Define/DefineApp.swift
@@ -0,0 +1,17 @@
+//
+//  DefineApp.swift
+//  Define
+//
+//  Created by Ben Shutt on 18/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct DefineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DefineUITests/DefineUITestsLaunchTests.swift
@@ -0,0 +1,36 @@
+//
+//  DefineUITestsLaunchTests.swift
+//  DefineUITests
+//
+//  Created by Ben Shutt on 18/09/2022.
+//
+
+import XCTest
+
+// swiftlint:disable empty_xctest_method
+
+class DefineUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
