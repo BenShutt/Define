416fda2f3fe38d55864588b95a1980b72b7f6a24
benshutt <ben@3sidedcube.com>
2023-04-27 21:30:38 +0100

    Share View extensions file

--- Define/Extensions/SwiftUI/View+Extensions.swift
+++ Define/Extensions/SwiftUI/View+Extensions.swift
@@ -27,4 +27,21 @@ extension View {
             .fixedSize(horizontal: false, vertical: true)
             .multilineTextAlignment(.leading)
     }
+
+    /// Implement a conditional modifier
+    ///
+    /// - Parameters:
+    ///   - condition: Condition of the `if` statement
+    ///   - transform: Mapping of `self` to some `View`
+    /// - Returns: `View`
+    @ViewBuilder func `if`<Transform: View>(
+        _ condition: Bool,
+        transform: (Self) -> Transform
+    ) -> some View {
+        if condition {
+            transform(self)
+        } else {
+            self
+        }
+    }
 }
