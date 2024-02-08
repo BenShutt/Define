e8be88ab07116a61e01bef7e806e649a0145b1db
Ben Shutt <shuttbenjamin@gmail.com>
2024-02-08 18:18:01 +0000

    Fix non-clickable items in DefinitionListItem

--- Define/Views/Components/List/DefinitionListItem.swift
+++ Define/Views/Components/List/DefinitionListItem.swift
@@ -14,37 +14,59 @@ struct DefinitionListItem: View {
     @State private var isExpanded = false
     var definition: Word.Meaning.Definition
 
+    var body: some View {
+        if definition.exampleTitle != nil {
+            Button(action: {
+                withAnimation {
+                    isExpanded.toggle()
+                }
+            }, label: {
+                DefinitionContentView(
+                    isExpanded: isExpanded,
+                    definition: definition
+                )
+            })
+        } else {
+            DefinitionContentView(
+                isExpanded: isExpanded,
+                definition: definition
+            )
+        }
+    }
+}
+
+// MARK: - DefinitionContentView
+
+private struct DefinitionContentView: View {
+
+    var isExpanded: Bool
+    var definition: Word.Meaning.Definition
+
     private var example: String? {
         definition.exampleTitle
     }
 
     var body: some View {
-        Button(action: {
-            withAnimation {
-                isExpanded.toggle()
-            }
-        }, label: {
-            HStack(spacing: .medium) {
-                VStack(alignment: .leading, spacing: .small) {
-                    Text(verbatim: definition.title)
-                        .body()
+        HStack(spacing: .medium) {
+            VStack(alignment: .leading, spacing: .small) {
+                Text(verbatim: definition.title)
+                    .body()
 
-                    if let example, isExpanded {
-                        Text("definition_example \(example)")
-                            .foregroundColor(.appBlue)
-                            .caption()
-                    }
-                }
-                .frame(maxWidth: .infinity, alignment: .leading)
-
-                if example != nil {
-                    Image(systemName: isExpanded ? "chevron.up" : "chevron.down")
-                        .font(.system(size: 12))
-                        .foregroundColor(Color.appDarkGray)
+                if let example, isExpanded {
+                    Text("definition_example \(example)")
+                        .foregroundColor(.appBlue)
+                        .caption()
                 }
             }
-            .listItem()
-        })
+            .frame(maxWidth: .infinity, alignment: .leading)
+
+            if example != nil {
+                Image(systemName: isExpanded ? "chevron.up" : "chevron.down")
+                    .font(.system(size: 12))
+                    .foregroundColor(Color.appDarkGray)
+            }
+        }
+        .listItem()
     }
 }
 
