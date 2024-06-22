691b6f453cdc3a9c0b0c6dacba8982443634dba2
Ben Shutt <ben.c.shutt@gmail.com>
2024-06-22 15:54:32 +0200

    Refactor AppIconGenerator

--- AppIcon/AppIconGenerator/Main.swift
+++ AppIcon/AppIconGenerator/Main.swift
@@ -5,31 +5,62 @@
 //  Created by Ben Shutt on 29/09/2023.
 //
 
-import Foundation
+import SwiftUI
+import AppIcon
 import ArgumentParser
 
 @main
 struct Main: AsyncParsableCommand {
-
     static let configuration = CommandConfiguration(
-        abstract: "Generate app icon images from a SwiftUI view"
+        abstract: "Generate images from SwiftUI views"
     )
 
-    @Option(help: "The path of the directory to write the images to")
+    @Option(help: "The file path of the directory to write the images to")
     var directory: String
 
-    var directoryURL: URL {
+    /// Get `URL` of the `directory` file path
+    private var directoryURL: URL {
         URL(filePath: (directory as NSString).expandingTildeInPath)
     }
 
-    mutating func run() async throws {
-#if canImport(Cocoa)
+    @MainActor mutating func run() async throws {
         try FileManager.default.createDirectory(
             at: directoryURL,
             withIntermediateDirectories: true
         )
-        try await AppIconGenerator(directoryURL: directoryURL).generate()
-        print("Success, app icon images written to '\(directoryURL)'")
-#endif
+
+        try ImageToWrite(
+            fileName: "AppIcon",
+            width: 1024,
+            height: 1024,
+            content: AppIcon(size: 1024, isContainer: false) // TODO: Make sep views for the flag
+        )
+        .renderAndWrite(in: directoryURL)
+
+        try ImageToWrite(
+            fileName: "LaunchScreen",
+            width: 1290, // iPhone 15 Pro Max sizing
+            height: 2796,
+            content: LaunchScreen()
+        )
+        .renderAndWrite(in: directoryURL)
+
+        print("Success, images written to '\(directoryURL)'")
+    }
+}
+
+// MARK: - ImageToWrite
+
+@MainActor private struct ImageToWrite<Content: View> {
+    var fileName: String
+    var width: CGFloat
+    var height: CGFloat
+    var content: Content
+
+    func renderAndWrite(in directoryURL: URL) throws {
+        let url = directoryURL.appendingPathComponent("\(fileName).png")
+        let renderer = PNGRenderer(width: width, height: height)
+        let data = try renderer.pngData(of: content)
+        try data.write(to: url, options: .atomic)
     }
 }
---
+++ AppIcon/AppIconGenerator/PNGRenderer.swift
@@ -0,0 +1,48 @@
+//
+//  PNGRenderer.swift
+//  AppIconGenerator
+//
+//  Created by Ben Shutt on 22/06/2024.
+//
+
+import SwiftUI
+
+// MARK: - PNGRenderer
+
+/// Renders a PNG image from a SwiftUI view
+@MainActor struct PNGRenderer {
+    var scale: CGFloat = 1
+    var width: CGFloat
+    var height: CGFloat
+
+    func pngData(of content: some View) throws -> Data {
+        let renderer = ImageRenderer(content: content)
+        renderer.scale = scale
+        renderer.proposedSize = .init(width: width, height: height)
+        let pngData = renderer.pngData()
+        guard let pngData else { throw PNGRendererError.pngData }
+        return pngData
+    }
+}
+
+// MARK: - PNGRendererError
+
+enum PNGRendererError: Error {
+    case pngData
+}
+
+// MARK: - ImageRenderer + Extensions
+
+private extension ImageRenderer {
+    @MainActor func pngData() -> Data? {
+#if os(iOS)
+        uiImage?.pngData()
+#elseif canImport(Cocoa)
+        guard let tiff = nsImage?.tiffRepresentation else { return nil }
+        guard let tiffData = NSBitmapImageRep(data: tiff) else { return nil }
+        return tiffData.representation(using: .png, properties: [:])
+#else
+        nil
+#endif
+    }
+}
