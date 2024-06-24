7c26ced412858e7935c474deb3ae22ebf1445c9d
Ben Shutt <ben.c.shutt@gmail.com>
2024-06-24 10:44:54 +0200

    Fix View+SizeChanged.swift after moving to package

--- AppIcon/AppIcon/Extensions/View+SizeChanged.swift
+++ AppIcon/AppIcon/Extensions/View+SizeChanged.swift
@@ -27,7 +27,7 @@ public extension View {
 
 // MARK: - SizePreferenceKey
 
-struct SizePreferenceKey: PreferenceKey {
+private struct SizePreferenceKey: PreferenceKey {
     static var defaultValue: CGSize = .zero
     static func reduce(value: inout CGSize, nextValue: () -> CGSize) {}
 }
