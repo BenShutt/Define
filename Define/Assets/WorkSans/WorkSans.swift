17a7091cdec962124acd5327d4f3224148f693f9
Ben Shutt <ben.c.shutt@gmail.com>
2024-06-25 11:16:15 +0200

    Update WorkSans raw values

--- Define/Assets/WorkSans/WorkSans.swift
+++ Define/Assets/WorkSans/WorkSans.swift
@@ -9,26 +9,24 @@ import Foundation
 
 /// WordSans custom font caes
 enum WorkSans: String, CustomFont {
-    case black
-    case blackItalic
-    case bold
-    case boldItalic
-    case extraBold
-    case extraBoldItalic
-    case extraLight
-    case extraLightItalic
-    case italic
-    case light
-    case lightItalic
-    case medium
-    case mediumItalic
-    case regular
-    case semiBold
-    case semiBoldItalic
-    case thin
-    case thinItalic
+    case black = "WorkSans-Black"
+    case blackItalic = "WorkSans-BlackItalic"
+    case bold = "WorkSans-Bold"
+    case boldItalic = "WorkSans-BoldItalic"
+    case extraBold = "WorkSans-ExtraBold"
+    case extraBoldItalic = "WorkSans-ExtraBoldItalic"
+    case extraLight = "WorkSans-ExtraLight"
+    case extraLightItalic = "WorkSans-ExtraLightItalic"
+    case italic = "WorkSans-Italic"
+    case light = "WorkSans-Light"
+    case lightItalic = "WorkSans-LightItalic"
+    case medium = "WorkSans-Medium"
+    case mediumItalic = "WorkSans-MediumItalic"
+    case regular = "WorkSans-Regular"
+    case semiBold = "WorkSans-SemiBold"
+    case semiBoldItalic = "WorkSans-SemiBoldItalic"
+    case thin = "WorkSans-Thin"
+    case thinItalic = "WorkSans-ThinItalic"
 
-    var fontName: String {
-        "WorkSans-\(rawValue.uppercasedFirstCharacter)"
-    }
+    var fontName: String { rawValue }
 }
