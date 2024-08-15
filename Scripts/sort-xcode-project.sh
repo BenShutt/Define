e2308e841afb18acaf748b229ba02abf49e4971c
Ben Shutt <ben.c.shutt@gmail.com>
2024-08-15 16:44:01 +0100

    Update Xcode sort script and update package dependencies

--- Scripts/sort-xcode-project.sh
+++ Scripts/sort-xcode-project.sh
@@ -11,6 +11,9 @@
 # Set defaults
 set -o nounset -o errexit -o errtrace -o pipefail
 
+# URL of the remote perl script on GitHub
+REMOTE_URL="https://raw.githubusercontent.com/WebKit/webkit/main/Tools/Scripts/sort-Xcode-project-file"
+
 # Get directory of this script
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
@@ -19,6 +22,9 @@ PROJECT_FILE="${SCRIPT_DIR}/../Define.xcodeproj/project.pbxproj"
 
 # Perl script to sort the file alphabetically
 PERL_SCRIPT="${SCRIPT_DIR}/alphabetically-sort-xcode-project.pl"
+
+# Update the local file
+curl -fsS -o "${PERL_SCRIPT}" "${REMOTE_URL}"
 
 # Run the script
 perl "${PERL_SCRIPT}" "${PROJECT_FILE}"
